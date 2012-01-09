1ea4dea1033fabeebb8dd96a69d27ded51360160
Sebastian Geiger <sebastian.geiger@gmail.com>
2012-01-09 13:26:11 +0100

    Added the all_messages method which is more generic than output. Added select, collect and filter to the Output class.

--- functional_test.rb
+++ functional_test.rb
@@ -1,9 +1,13 @@
-def output
+def all_messages
   stdin = []
   while line=STDIN.gets do
-    stdin << line if line =~ /output>>/
+    stdin << line 
   end
   Output.new(stdin)
+end
+
+def output
+  all_messages.select{|line| line =~ /output>>/}
 end
 
 class Output
@@ -12,6 +16,15 @@ class Output
   end
   def interpreted_as_numeric_array
     return @array.collect{|line| line.gsub(/$[\n|\r|\r\n]/, "").gsub(/output>>/,"").strip.to_i}
+  end
+  def select(&block)
+    Output.new(@array.select &block)
+  end
+  def collect(&block)
+    Output.new(@array.collect &block)
+  end
+  def filter(regexp, capture_group = 1)
+    self.select{|line| line =~ regexp}.collect{|line| regexp.match(line)[capture_group]}
   end
 end
 
@@ -37,11 +50,13 @@ EMBEDDED_RUBY_LINE_START = /^\s*\/\/RUBY:/
 
 def find_embedded_ruby(file)
   embedded_ruby = File.open(file, "r+"){|file_handle|
-    file_handle.read.split(/[\n|\r|\r\n]/)
+    file_handle.read.split(/[\r\n]/)
   }.select{|line| line =~ EMBEDDED_RUBY_LINE_START}
   raise "More than one line of embedded ruby found" if embedded_ruby.size > 1
   raise "No embedded ruby found, needs to start with \"#{EMBEDDED_RUBY_LINE_START}\"" if embedded_ruby.size <= 0
   embedded_ruby.first.gsub(EMBEDDED_RUBY_LINE_START, "").strip
 end
 
-eval(find_embedded_ruby(ARGV.first))
+if __FILE__ == $0
+  eval(find_embedded_ruby(ARGV.first))
+end
